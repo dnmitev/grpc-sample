0598ba6baa7fb4dbdaa616dc57945e66587031ff
Dimitar Mitev <Dimitar.Mitev@experian.com>
2022-02-11 15:26:30 +0200

    added initial code

---
+++ GrpcSampleClient/GrpcSampleClient/Controllers/GreetingController.cs
@@ -0,0 +1,32 @@
+using Grpc.Net.Client;
+
+using GrpcService1;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcSampleClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreetingController : ControllerBase
+    {
+        private readonly ILogger<GreetingController> _logger;
+
+        public GreetingController(ILogger<GreetingController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpGet(Name = "GetGreeted")]
+        public async Task<string> Get(string name)
+        {
+            using var channel= GrpcChannel.ForAddress("https://localhost:49153");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = name });
+
+            return reply.Message;
+        }
+    }
+}
